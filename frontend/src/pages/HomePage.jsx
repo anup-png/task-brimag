a6ae04df05c4916d302caf56e863feb33a692e8e
anup-png <anup.202104091@tulas.edu.in>
2025-12-01 19:28:23 +0530

    frontend basic setup is done

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import OrderList from '../components/OrderList'
+
+const HomePage = () => {
+  return (
+    <div>
+      <OrderList/>
+    </div>
+  )
+}
+
+export default HomePage
