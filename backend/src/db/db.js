f8429fd1474908fc746566f7dadd4b8f1765547c
anup-png <anup.202104091@tulas.edu.in>
2025-12-02 11:18:03 +0530

    updated db

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,21 +1,41 @@
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
+// import { PrismaPg } from '@prisma/adapter-pg';
+// import pg from 'pg';
+
+
+
+// const pool = new pg.Pool({ 
+//   connectionString: process.env.DATABASE_URL 
+// });
+
+
+
+// const adapter = new PrismaPg(pool);
+// const prisma = new PrismaClient({ adapter });
+
+// // Test query
+// prisma.$connect()
+//   .then(() => console.log('Prisma connected successfully'))
+//   .catch(err => console.error('Prisma connection error:', err));
+
+// export default prisma;
+
+
+import pkg from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import pg from 'pg';
 
+const { PrismaClient } = pkg;
 
-
-const pool = new pg.Pool({ 
-  connectionString: process.env.DATABASE_URL 
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false } // Required for Render + Neon
 });
-
-
 
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
-// Test query
-prisma.$connect()
-  .then(() => console.log('Prisma connected successfully'))
-  .catch(err => console.error('Prisma connection error:', err));
+// Prevent multiple client instances in Production (Render/Vercel)
+if (process.env.NODE_ENV !== 'production') globalThis.prisma ??= prisma;
 
 export default prisma;
